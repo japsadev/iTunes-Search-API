f7767faab36346e264b8821f9076e581f65799be
Mehmet ateş <Mehmet1095@icloud.com>
2022-01-22 15:45:37 +0300

    Initial commit

---
+++ CustomiTunes/CustomiTunesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomiTunesApp.swift
+//  CustomiTunes
+//
+//  Created by Mehmet Ateş on 22.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct CustomiTunesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
