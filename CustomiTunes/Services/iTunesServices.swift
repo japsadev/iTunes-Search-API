9a0ce4460d29abd03177e67ddeb996e725bc7b98
Mehmet Ateş  <Mehmet1095@icloud.com>
2022-06-12 18:55:29 +0300

    the client completely transferred to the service and deleted

--- CustomiTunes/Services/iTunesServices.swift
+++ CustomiTunes/Services/iTunesServices.swift
@@ -8,14 +8,14 @@
 import Foundation
 
 struct ItunesServices{
-    let serviceURL = "https://itunes.apple.com"
+    private let serviceURL = "https://itunes.apple.com"
     
     // MARK: Helper methods
-    func createRequestUrl(endpoint: String, query: String?) -> URL?{
+    private func createRequestUrl(endpoint: String, query: String?) -> URL?{
         return URL(string: self.serviceURL + endpoint + (query ?? ""))
     }
     
-    func handleProperties(_ key: String) -> String{
+    private func handleProperties(_ key: String) -> String{
         var resultKey = key.replacingOccurrences(of: " ", with: "+").lowercased()
         
         let turkishChar = ["ü" , "ı", "ğ", "ş", "ç" , "ö"]
@@ -28,28 +28,29 @@ struct ItunesServices{
         return resultKey
     }
     
-    // MARK: Request methods
+    // MARK: Request method
     func searchByNameOrId(_ nameOrId: String, limit: Int? = 10, isAblum: Bool? = false, completion: @escaping(Result<Any?, DownloadError>) -> Void){
         let id: Double? = Double(nameOrId)
         var requestURL: URL?
         
         if let id = id {
-            let query: String = isAblum! ? "?id=\(id)&entity=album&limit=\(limit!)&sort=recent&country=TR" : "?id=\(id)&entity=song&limit=\(limit!)&sort=recent&country=TR"
-            guard let handledURL = createRequestUrl(endpoint: "/lookup", query: query) else { return completion(.failure(.wrongUrl))}
+            let formattedId = Int(id)
+            let query: String = isAblum! ? "?id=\(formattedId)&entity=album&limit=\(limit!)&sort=recent&country=TR" : "?id=\(formattedId)&entity=song&limit=\(limit!)&sort=recent&country=TR"
+            guard let handledURL = createRequestUrl(endpoint: "/lookup", query: query) else { return completion(.failure(.wrongUrl(url: requestURL?.path ?? "")))}
             requestURL = handledURL
         }else{
             let handledName = handleProperties(nameOrId)
-            guard let handledURL = createRequestUrl(endpoint: "/search", query: "?term=\(handledName)&limit=\(limit!)&entity=song&sort=recent&country=TR") else { return completion(.failure(.wrongUrl))}
+            guard let handledURL = createRequestUrl(endpoint: "/search", query: "?term=\(handledName)&limit=\(limit!)&entity=song&sort=recent&country=TR") else { return completion(.failure(.wrongUrl(url: requestURL?.path ?? "")))}
             requestURL = handledURL
         }
         
         URLSession.shared.dataTask(with: requestURL!) { responseData, response, error in
-            guard let responseData = responseData else { return completion(.failure(.unload)) }
+            guard let responseData = responseData else { return completion(.failure(.unload(url: requestURL?.path ?? ""))) }
             if isAblum!{
-                guard let handledData = try? JSONDecoder().decode(SearchAlbumData.self, from: responseData) else { return completion(.failure(.unbuild))}
+                guard let handledData = try? JSONDecoder().decode(SearchAlbumData.self, from: responseData) else { return completion(.failure(.unbuild(url: requestURL?.path ?? "")))}
                 return completion(.success(handledData.results))
             }else{
-                guard let handledData = try? JSONDecoder().decode(SearchSongData.self, from: responseData) else { return completion(.failure(.unbuild))}
+                guard let handledData = try? JSONDecoder().decode(SearchSongData.self, from: responseData) else { return completion(.failure(.unbuild(url: requestURL?.path ?? "")))}
                 return completion(.success(handledData.results))
             }
         }.resume()
