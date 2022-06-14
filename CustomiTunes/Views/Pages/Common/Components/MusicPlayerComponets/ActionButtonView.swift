e662736c3e9933c4a7a9e8f8e4451174f6c9b8f1
Mehmet Ateş  <Mehmet1095@icloud.com>
2022-06-14 14:35:24 +0300

    ActionButton appearance completely revamped

--- CustomiTunes/Views/Pages/Common/Components/MusicPlayerComponets/ActionButtonView.swift
+++ CustomiTunes/Views/Pages/Common/Components/MusicPlayerComponets/ActionButtonView.swift
@@ -31,9 +31,20 @@ struct ActionButtonView: View {
             HStack{
                 Text(self.isValue ? LocalizedStringKey(activeTitle) : LocalizedStringKey(disActiveTitle))
                 Image(systemName: self.isValue ? activeIcon : disActiveIcon)
-            }.padding(10)
+            }.padding()
                 .frame(width: self.buttonWidth, height: 5.0.responsiveH)
-                .overlay(RoundedRectangle(cornerRadius: 10).stroke())
+                .foregroundColor(isValue ? .white : .accentColor)
+                .overlay(isValue ? nil : RoundedRectangle(cornerRadius: 10).stroke())
+                .background(isValue ? RoundedRectangle(cornerRadius: 10) : nil)
+                .font(.footnote)
+        }
+    }
+}
+
+struct ActionButtonView_Preview: PreviewProvider{
+    static var previews: some View{
+        ActionButtonView(isValue: .constant(true), activeIcon: "play", disActiveIcon: "pause", activeTitle: "Play", disActiveTitle: "Pause") {
+            
         }
     }
 }
