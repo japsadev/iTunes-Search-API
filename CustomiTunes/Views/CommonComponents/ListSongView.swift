4873211d4b1f3cd3cac41a59b51c0979fc655256
Mehmet Ateş  <Mehmet1095@icloud.com>
2022-06-20 15:37:48 +0300

    List song view moved to common components

---
+++ CustomiTunes/Views/CommonComponents/ListSongView.swift
@@ -0,0 +1,67 @@
+//
+//  ListSongView.swift
+//  CustomiTunes
+//
+//  Created by Mehmet Ateş on 2.02.2022.
+//
+
+import SwiftUI
+
+struct ListSongView: View {
+    private let contentSize: CGFloat = 15.0.responsiveW
+    var songData: SongData
+    
+    var body: some View {
+        NavigationLink {
+            SongDetailView(songId: songData.wrappedId)
+        } label: {
+            VStack(alignment:.leading,spacing:5){
+                HStack{
+                    AnimatedAsyncImageView(imageURL: songData.smallImageURL) { image in
+                        image
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: contentSize, height: contentSize, alignment: .center)
+                            .clipped()
+                            .cornerRadius(10)
+                    } errorView: {
+                        Image(systemName: "xmark")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: contentSize, height: contentSize, alignment: .center)
+                            .clipped()
+                            .cornerRadius(10)
+                            .background(
+                                Rectangle()
+                                    .frame(width: contentSize, height: contentSize)
+                                    .cornerRadius(10)
+                                    .foregroundColor(.secondary.opacity(0.3))
+                            )
+                    } placeHolderView: {
+                        ProgressView()
+                            .background(
+                                Rectangle()
+                                    .frame(width: contentSize, height: contentSize)
+                                    .cornerRadius(10)
+                                    .foregroundColor(.secondary.opacity(0.3))
+                            )
+                    }.frame(width: contentSize, height: contentSize, alignment: .center)
+                    
+                    VStack(alignment:.leading){
+                        HStack{
+                            Text(songData.wrappedTrackName)
+                                .font(.title3)
+                            ExplicitView(explicitState: songData.wrappedTrackExplicitness)
+                        }
+                        Text(songData.wrappedArtistName)
+                            .font(.callout)
+                            .foregroundColor(.secondary)
+                        Spacer()
+                    }
+                }
+                Divider()
+            }.frame(width: 90.0.responsiveW, height: 15.0.responsiveW, alignment: .center)
+                .foregroundColor(.primary)
+        }
+    }
+}
