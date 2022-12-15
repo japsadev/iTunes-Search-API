6882c3293239c6be7ec94525108c39eae454cd1b
Mehmet Ateş  <Mehmet1095@icloud.com>
2022-12-15 20:01:03 +0300

    refactoring on services

--- CustomiTunes/Services/FavoriteService.swift
+++ CustomiTunes/Services/FavoriteService.swift
@@ -7,56 +7,55 @@
 
 import Foundation
 
-class FavoriteService: ObservableObject{
+class FavoriteService: ObservableObject {
     private let saveKey = "favoriteTracks"
-    @Published var favoriteTrackIds: Array<Double> = []{
-        didSet{
+    @Published var favoriteTrackIds: [Double] = [] {
+        didSet {
             saveFavorites()
         }
     }
-    
-    func saveFavorites(){
-        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first{
+
+    func saveFavorites() {
+        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
             let fileURL = dir.appendingPathComponent(saveKey)
-            
-            do{
+
+            do {
                 if let encoded = try? JSONEncoder().encode(favoriteTrackIds) {
                     try encoded.write(to: fileURL)
                 }
-            } catch{ print("save error") }
+            } catch { print("save error") }
         }
     }
-    
-    func readData(){
-        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first{
+
+    func readData() {
+        if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
             let fileURL = dir.appendingPathComponent(saveKey)
-            
-            do{
+
+            do {
                 let stringContent = try String(contentsOf: fileURL, encoding: .utf8)
                 if let decoded = try? JSONDecoder().decode([Double].self, from: stringContent.data(using: .utf8) ?? Data()) {
                     self.favoriteTrackIds = decoded
                 }
-            } catch{ print("read error"); self.favoriteTrackIds = [] }
+            } catch { print("read error"); self.favoriteTrackIds = [] }
         }
     }
-    
-    func addOrRemoveFavorite(_ id: Double){
+
+    func addOrRemoveFavorite(_ id: Double) {
         let isContains = favoriteTrackIds.contains { $0 == id }
-        if isContains{
-            if let firstIndex = favoriteTrackIds.firstIndex(of: id){
+        if isContains {
+            if let firstIndex = favoriteTrackIds.firstIndex(of: id) {
                 favoriteTrackIds.remove(at: firstIndex)
             }
-        }else{
+        } else {
             favoriteTrackIds.append(id)
         }
     }
-    
-    func isFavorite(_ id: Double) -> Bool{
+
+    func isFavorite(_ id: Double) -> Bool {
         favoriteTrackIds.contains { $0 == id }
     }
-    
-    init(){
+
+    init() {
         readData()
     }
-    
 }
